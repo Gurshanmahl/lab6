b1fee8be07de62b8fb11a47b7bc65199b44042e5
Gurshanmahl <gurshanmahl@uvic.ca>
2024-03-02 14:56:34 -0800

    git add . git commit -m "finished before_birthdates.py"

--- q_my_labs.py
+++ q_my_labs.py
@@ -14,15 +14,17 @@ def main():
     """
 
     # COMPLETE IMPLEMENTATION
-    todays_date = None
+    todays_date = datetime.datetime(2024, 2, 28)
+    print(todays_date)
 
     date_list = every_lab(todays_date)
-
     """ 
     variable date_list should contain datetime objects 
     for all the days when you have a lab
     print these dates in the format "Mon, 15 Jan 21"
     """
+    for dates in date_list:
+        print(dates)
 
     # COMPLETE IMPLEMENTATION
 
@@ -35,6 +37,16 @@ def every_lab(todays_date):
     This function will create datetimes objects for those labs, 
     add them to a list and then return this list
     """
+    lab_date = todays_date
+    weeks_left = 5
+    lab_date_list = []
+
+    while weeks_left >= 0:
+        future_lab = lab_date.strftime("%a, %d %b %y")
+        lab_date_list.append(future_lab)
+        lab_date += datetime.timedelta(days=7)
+        weeks_left -= 1
+    return lab_date_list
 
     # COMPLETE IMPLEMENTATION
 
